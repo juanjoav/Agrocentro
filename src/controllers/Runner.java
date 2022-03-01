b37d0ec1d7d906b67a8452e3ddd87d79979f5f6a
Juan Ariza <juanjoav010@gmail.com>
2022-03-01 07:20:04 -0500

    Initial commi

---
+++ src/controllers/Runner.java
@@ -0,0 +1,14 @@
+/*
+ * Copyright (c) 19/2/2022.
+ * Created by Juan Jose Ariza Velasco and Brayan Alfonso Collazos Ariza
+ * All rights reserved
+ */
+
+package controllers;
+
+public class Runner {
+
+    public static void main(String[] args) {
+        new Runner();
+    }
+}
---
+++ src/models/Inventory.java
@@ -0,0 +1,10 @@
+/*
+ * Copyright (c) 19/2/2022.
+ * Created by Juan Jose Ariza Velasco and Brayan Alfonso Collazos Ariza
+ * All rights reserved
+ */
+
+package models;
+
+public class Inventory {
+}
