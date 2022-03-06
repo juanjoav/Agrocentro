6506cd63bc87f2d18da5ecf1b2e2041b82e97880
juanj <juan.ariza07@uptc.edu.co>
2022-03-06 13:46:21 -0500

    El pdf soporta tablas, bugs presentes en la tabla del pdf

--- src/models/Billing.java
+++ src/models/Billing.java
@@ -7,55 +7,118 @@
 package models;
 
 import com.itextpdf.text.*;
-
+import com.itextpdf.text.Image;
+import com.itextpdf.text.pdf.PdfPCell;
+import com.itextpdf.text.pdf.PdfPTable;
 import com.itextpdf.text.pdf.PdfWriter;
+import models.Products.Product;
 
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.util.ArrayList;
 
 //aqui se hace el pdf
 public class Billing {
 
-    String name;
-    String apellido;
-    String fecha;
+    private Client client;
+    private String date;
+    private static int number = 0;
+    private ArrayList<Product> products;
+    private String imageRoute;
 
     Document document;
     FileOutputStream file;
     Paragraph title;
 
-    public Billing(String name, String apellido, String fecha) {
-        this.name = name;
-        this.apellido = apellido;
-        this.fecha = fecha;
+    public Billing(Client client, String date, ArrayList<Product> products, String imageRoute) {
+        this.client = client;
+        this.date = date;
+        this.products = products;
+        ++number;
+        this.imageRoute = imageRoute;
 
         document = new Document();
-        title = new Paragraph("Factura");
+        title = new Paragraph("Factura de venta");
     }
 
     public void buildInvoice(){
         try {
-            file = new FileOutputStream(name + ".pdf");
+            file = new FileOutputStream("Factura" + client.getName() + ".pdf");
             PdfWriter.getInstance(document, file);
             document.open();
             title.setAlignment(1);
             document.add(title);
 
-            document.add(new Paragraph("nombre: " + name));
-            document.add(new Paragraph("apellido: " + apellido));
+            document.add(loadImage());
+
+            document.add(new Paragraph("N° factura" + number));
+            document.add(new Paragraph("Comprador"));
+            document.add(Chunk.NEWLINE);
+            document.add(new Paragraph("nombre: " + client.getName()));
+            document.add(new Paragraph("apellido: " + client.getLastName()));
 
             document.add(Chunk.NEWLINE);
 
-            Paragraph text = new Paragraph("ESTO ES UN TEXT");
-            text.setAlignment(Element.ALIGN_JUSTIFIED);
-            document.add(text);
+
+            document.add(buildTable());
+
             document.add(Chunk.NEWLINE);
 
-            document.add(new Paragraph("Fecha: " + fecha));
+            document.add(new Paragraph("Fecha: " + date));
 
             document.close();
         } catch (FileNotFoundException | DocumentException e) {
             e.printStackTrace();
         }
+    }
+
+    private Image loadImage(){
+        Image image = null;
+        try {
+            image = Image.getInstance(imageRoute);
+            image.scaleAbsolute(150, 100);
+            image.setAbsolutePosition(415, 750);
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        } catch (BadElementException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return image;
+    }
+
+    private PdfPTable buildTable(){
+
+        PdfPTable table = new PdfPTable(4);
+        table.setWidthPercentage(100);
+
+        PdfPCell productName = new PdfPCell(new Phrase("Producto"));
+        productName.setBackgroundColor(BaseColor.ORANGE);
+
+        PdfPCell productPrice = new PdfPCell(new Phrase( "Precio"));
+        productName.setBackgroundColor(BaseColor.ORANGE);
+
+        PdfPCell productExpiration = new PdfPCell(new Phrase( "Expiracion"));
+        productName.setBackgroundColor(BaseColor.ORANGE);
+
+        PdfPCell productType = new PdfPCell(new Phrase( "Producto para"));
+        productName.setBackgroundColor(BaseColor.ORANGE);
+
+        table.addCell(productName);
+        table.addCell(productPrice);
+        table.addCell(productExpiration);
+        table.addCell(productType);
+
+        for (Product p: products) {
+            table.addCell(p.getName());
+            table.addCell("" + p.getPrice());
+            table.addCell("NO SUPPORTED");
+            table.addCell(p.getProductType());
+        }
+
+        return table;
     }
 }
--- src/test/TestBilling.java
+++ src/test/TestBilling.java
@@ -5,11 +5,27 @@
  */
 
 import models.Billing;
+import models.Client;
+import models.DocumentType;
+import models.Products.Product;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
 
 public class TestBilling {
 
     public static void main(String[] args) {
-        Billing billing = new Billing("juan","ariza", "09-10-22");
+
+        ArrayList<Product> products = new ArrayList<Product>();
+        Product product = new Product("jugo", 200, 4, LocalDate.of(2025,12,24),"perro");
+        Product product1 = new Product("purina", 200, 4, LocalDate.of(2025,12,24),"perro");
+        products.add(product);
+        products.add(product1);
+
+        Client client = new Client("Juan", "Ariza", DocumentType.CC, 1007451304);
+        Billing billing = new Billing(client,"09-10-22",products, "C:/Users/juanj/OneDrive/Documentos/Semestre V/Requisitos/Agrocentro/src/imgs/logo-agrocentro.png");
+//        Billing billng = new Billing("pepe","kk", "09-10-22", new String[]{"purina", "maiz", "arena"});
         billing.buildInvoice();
+       // billng.buildInvoice();
     }
 }
